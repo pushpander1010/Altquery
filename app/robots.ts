52e08fe8fa13a21c745eeef0b6b067ac4672762c
pushpander1010 <2023mt93337@wilp.bits-pilani.ac.in>
2025-10-03 22:48:22 +0530

    init commit

--- app/robots.ts
+++ app/robots.ts
@@ -2,11 +2,38 @@ import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/admin/', '/api/'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/api/debug',
+          '/admin/',
+          '/_next/',
+          '/setup/'
+        ],
+      },
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+        disallow: [
+          '/api/debug',
+          '/admin/',
+          '/_next/',
+          '/setup/'
+        ],
+      },
+      {
+        userAgent: 'Bingbot',
+        allow: '/',
+        disallow: [
+          '/api/debug',
+          '/admin/',
+          '/_next/',
+          '/setup/'
+        ],
+      }
+    ],
     sitemap: 'https://altquery.com/sitemap.xml',
   }
 }
